4ccdba4141d2b510c95fc4982d180724154a9679
Chanwon-Seo <scwonn60@gmail.com>
2024-05-14 20:46:17 +0900

    init spring commit

---
+++ src/test/java/com/scw/springtodomanagement/SpringTodoManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scw.springtodomanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
