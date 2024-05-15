e083b55c65f5d26ea37cf3f3eccb1418e2923924
Chanwon-Seo <scwonn60@gmail.com>
2024-05-15 19:22:29 +0900

    FEAT : Api 요청에 동작 중 예외가 발생한 경우 ApiException에 관리 및 handler를 통한 예외 처리 - ApiException : 로직 동작 중 예외가 발생한 경우 관리 - ApiExceptionHandler : ApiException 발생 한 경우 처리

---
+++ src/main/java/com/scw/springtodomanagement/common/exceptionhadler/ApiExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.scw.springtodomanagement.common.exceptionhadler;
+
+import com.scw.springtodomanagement.common.errorcode.ErrorCode;
+import com.scw.springtodomanagement.common.exception.ApiException;
+import com.scw.springtodomanagement.common.global.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@Order(1)
+@RestControllerAdvice
+public class ApiExceptionHandler {
+    /**
+     * Api 요청에 동작 중 예외가 발생한 경우
+     */
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ErrorResponse> apiException(ApiException e) {
+        log.error("", e);
+
+        ErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity.status(errorCode.getHttpStatusCode())
+                .body(ErrorResponse.of(errorCode));
+    }
+
+}
