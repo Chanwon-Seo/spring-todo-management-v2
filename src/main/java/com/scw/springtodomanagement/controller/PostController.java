12d5b0ce1b019c2ef030964f171b84b484cfc3ef
Chanwon-Seo <scwonn60@gmail.com>
2024-05-15 18:06:40 +0900

    FEAT : PostController 응답 결과 변경 및 DTO에 PostCURequestDTO 유효성 검증(Valid) - PostController : RestApiResponse에 값을 담아서 응답한다. - PostCURequestDto : 유효성 검증 추가

--- src/main/java/com/scw/springtodomanagement/controller/PostController.java
+++ src/main/java/com/scw/springtodomanagement/controller/PostController.java
@@ -1,15 +1,20 @@
 package com.scw.springtodomanagement.controller;
 
+import com.scw.springtodomanagement.common.global.response.RestApiResponse;
 import com.scw.springtodomanagement.controller.request.PostCURequestDTO;
+import com.scw.springtodomanagement.controller.request.PostDRequestDTO;
 import com.scw.springtodomanagement.controller.response.PostResponseDTO;
 import com.scw.springtodomanagement.service.PostService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
+
+import static com.scw.springtodomanagement.common.errorcode.StatusCode.*;
 
 @Tag(name = "02. Post")
 @Slf4j
@@ -21,39 +26,50 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public PostResponseDTO createPost(
-            @RequestBody PostCURequestDTO requestDTO
+    public ResponseEntity<RestApiResponse<PostResponseDTO>> createPost(
+            @Valid @RequestBody PostCURequestDTO requestDTO
     ) {
-        return postService.createPost(requestDTO);
+        PostResponseDTO savePost = postService.createPost(requestDTO);
+
+        return ResponseEntity.status(OK.code)
+                .body(RestApiResponse.of(savePost));
     }
 
     @GetMapping("/{id}")
-    public PostResponseDTO findPostById(
+    public ResponseEntity<RestApiResponse<PostResponseDTO>> findPostById(
             @PathVariable Long id
     ) {
-        return postService.findPostById(id);
+        PostResponseDTO findPost = postService.findPostById(id);
+
+        return ResponseEntity.status(OK.code)
+                .body(RestApiResponse.of(findPost));
     }
 
     @GetMapping
-    public List<PostResponseDTO> findAll() {
-        return postService.findAll();
+    public ResponseEntity<RestApiResponse<List<PostResponseDTO>>> findAll() {
+        List<PostResponseDTO> findAllPost = postService.findAll();
+        return ResponseEntity.status(OK.code)
+                .body(RestApiResponse.of(findAllPost));
     }
 
     @PutMapping("/{id}")
-    public PostResponseDTO updatePost(
+    public ResponseEntity<RestApiResponse<PostResponseDTO>> updatePost(
             @PathVariable Long id,
-            @RequestBody PostCURequestDTO requestDTO
+            @Valid @RequestBody PostCURequestDTO requestDTO
     ) {
-        return postService.updatePost(id, requestDTO);
+        PostResponseDTO updatePost = postService.updatePost(id, requestDTO);
+        return ResponseEntity.status(OK.code)
+                .body(RestApiResponse.of(updatePost));
     }
 
     @DeleteMapping("/{id}")
-    public String deletePost(
+    public ResponseEntity<RestApiResponse<Object>> deletePost(
             @PathVariable Long id,
-            @RequestBody Map<String, String> requestPassword
+            @RequestBody PostDRequestDTO requestDTO
     ) {
-        postService.deletePost(id, requestPassword);
+        postService.deletePost(id, requestDTO);
 
-        return "삭제되었습니다.";
+        return ResponseEntity.status(OK.code)
+                .body(RestApiResponse.of("삭제되엇습니다."));
     }
 }
