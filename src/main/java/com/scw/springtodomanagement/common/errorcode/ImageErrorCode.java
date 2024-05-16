340e0506b0997261560f6e7482eb4be82a7c22c1
Chanwon-Seo <scwonn60@gmail.com>
2024-05-17 02:33:41 +0900

    FEAT : 다운로드 요청 시 이미지가 저장되어 있지 않은 경우 예외 메시지 수정 - 기존 => 존재하지 않는 게시물입니다. - 수정 => 존재하지 않는 이미지입니다.

--- src/main/java/com/scw/springtodomanagement/common/errorcode/ImageErrorCode.java
+++ src/main/java/com/scw/springtodomanagement/common/errorcode/ImageErrorCode.java
@@ -8,8 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum ImageErrorCode implements ErrorCode {
 
     UNSUPPORTED_IMAGE_EXTENSION(StatusCode.BAD_REQUEST.getCode(), "지원하지 않는 확장자입니다."),
-    NOT_FOUND_IMAGE_FILE(StatusCode.BAD_REQUEST.getCode(), "존재하지 않는 게시물입니다.");
-    ;
+    NOT_FOUND_IMAGE_FILE(StatusCode.BAD_REQUEST.getCode(), "존재하지 않는 이미지입니다.");;
 
 
     private final Integer httpStatusCode;
