57fd6ec4b4b41a6725282e3187ab9d2663d3fc1a
Chanwon-Seo <scwonn60@gmail.com>
2024-05-17 02:31:59 +0900

    FEAT : Image Upload시 응답 결과 수정 - 기존 => 원본 파일명, UUID 다운로드 링크 - 수정 => 원본 파일명, UUID 파일명, UUID 다운로드 링크

--- src/main/java/com/scw/springtodomanagement/domain/controller/response/image/ImageResponseDTO.java
+++ src/main/java/com/scw/springtodomanagement/domain/controller/response/image/ImageResponseDTO.java
@@ -1,4 +1,4 @@
-package com.scw.springtodomanagement.domain.controller.response;
+package com.scw.springtodomanagement.domain.controller.response.image;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -8,9 +8,11 @@ import lombok.Getter;
 public class ImageResponseDTO {
     private String uploadFileName;
     private String UUIDFileName;
+    private String downloadURL;
 
-    public ImageResponseDTO(String uploadFileName, String UUIDFileName) {
+    public ImageResponseDTO(String uploadFileName, String UUIDFileName, String downloadURL) {
         this.uploadFileName = uploadFileName;
         this.UUIDFileName = UUIDFileName;
+        this.downloadURL = downloadURL;
     }
 }
